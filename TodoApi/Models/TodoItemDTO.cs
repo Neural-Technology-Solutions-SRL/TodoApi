32523f72af47b08c11c6d460474e92bfefc8d5d8
Simon Garcia <sgarcia@neural.com.do>
2022-09-26 16:06:13 -0400

    Added TodoItemDTO

---
+++ TodoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItemDTO
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
